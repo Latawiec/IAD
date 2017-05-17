596bbb55ec133ab5dbafa3c628dd782110140a8d
Latawiec <Latawiec@DESKTOP-6I1PLFP>
2017-05-17 14:30:17 +0200

    Added Neuron class.

--- Kmeans/Main.cpp
+++ Kmeans/Main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <string>
+#include "Neuron.h"
+
 
 void main() {
+	Neuron<int> a({ 5, 3, 5 });
+
 	std::cout << "Hello world!" << std::endl;
+	std::cout << a.printWeights() << std::endl;
+	std::getchar();
 }
---
+++ Kmeans/Neuron.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <sstream>
+
+template <typename T>
+class Neuron
+{
+public:
+	Neuron(std::initializer_list<T> &&values) : weights_(std::move(values)) {};
+	Neuron(std::vector<T> &&values) : weights_(std::move(values)) {};
+	Neuron(const std::vector<T> &values) : weights_(values) {};
+	virtual	~Neuron() {};
+
+	constexpr const std::vector<T>& getWeights()const { return weights_ };
+	void setWeights(const std::vector<T> &values) { weights_ = std::copy(values) };
+	std::string printWeights()const {
+		std::ostringstream str;
+		for (const T& val : weights_) {
+			str << val << '\t';
+		}
+		return str.str();
+	}
+
+private:
+	std::vector<T> weights_;
+};
+
